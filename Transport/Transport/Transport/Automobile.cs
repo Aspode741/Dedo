7e93ae881c1bc6e2c4ab9ee5631ba3875adfe373
Aspode741 <vsarov356@gmail.com>
2026-05-27 00:18:13 +0500

    15 задание

--- Transport/Transport/Transport/Automobile.cs
+++ Transport/Transport/Transport/Automobile.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Transport
 {
-    public abstract class Automobile
+    public abstract class Automobile : IComparable<Automobile>
     {
         public Registration Registration { get; set; }
         public int EnginePower { get; set; }
@@ -20,5 +20,20 @@ namespace Transport
         }
 
         public abstract string[] GetInfo();
+
+        // сортировка по марке/модели, потом по рег знаку
+        public int CompareTo(Automobile other)
+        {
+            if (other == null)
+                return 1;
+
+            // сравниваем модели 
+            int modelCompare = string.Compare(this.Registration.Model, other.Registration.Model, StringComparison.OrdinalIgnoreCase);
+            if (modelCompare != 0)
+                return modelCompare;
+
+            // если модели одинаковые, то сравниваем знаки
+            return string.Compare(this.Registration.RegistrationPlate, other.Registration.RegistrationPlate, StringComparison.OrdinalIgnoreCase);
+        }
     }
 }
